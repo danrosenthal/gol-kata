c4d99e4c269a60eb7e9fb7da4c8cb89aff6b4fc7
Dan Rosenthal <dan@heysparkbox.com>
2015-03-23 16:02:06 -0400

    feat: initialize witout coordinates

--- lib/neighborhood.rb
+++ lib/neighborhood.rb
@@ -1,47 +1,45 @@
+require 'cell.rb'
+
 class Neighborhood
-  attr_reader :grid, :x, :y
-  def initialize(grid, x, y)
-    @x = x
-    @y = y
+  attr_reader :grid, :x, :y, :live_state
+  def initialize(grid)
     @grid = grid
-  end
-  def number_of_living_neighbors
-    neighbor_states = []
-    get_neighbors.each do |x, y|
-      neighbor_states << grid[x][y]
-    end
-    neighbor_states.reduce(:+)
+    @live_state = Cell.new.live_state
   end
 
-  def get_neighbors
-    neighbors = [find_northern_neighbor, find_northernwestern_neighbor,
-                 find_western_neighbor, find_southwestern_neighbor,
-                 find_southern_neighbor, find_southeastern_neighbor,
-                 find_eastern_neighbor, find_northeastern_neighbor].compact
+  def number_of_living_neighbors(x,y)
+    get_neighbors(x,y).map { |x, y| grid[x][y] } .count(live_state)
   end
 
-  def find_northern_neighbor
+  def get_neighbors(x,y)
+    neighbors = [find_northern_neighbor(x,y), find_northernwestern_neighbor(x,y),
+                 find_western_neighbor(x,y), find_southwestern_neighbor(x,y),
+                 find_southern_neighbor(x,y), find_southeastern_neighbor(x,y),
+                 find_eastern_neighbor(x,y), find_northeastern_neighbor(x,y)].compact
+  end
+
+  def find_northern_neighbor(x,y)
     [x-1, y] if (x-1 >= 0)
   end
-  def find_northernwestern_neighbor
+  def find_northernwestern_neighbor(x,y)
     [x-1, y-1] if (x-1 >= 0 && y-1 >=0)
   end
-  def find_western_neighbor
+  def find_western_neighbor(x,y)
     [x, y-1] if (y-1 >= 0)
   end
-  def find_southwestern_neighbor
+  def find_southwestern_neighbor(x,y)
     [x+1, y-1] if (y-1 >= 0 && x+1 < grid.length)
   end
-  def find_southern_neighbor
+  def find_southern_neighbor(x,y)
     [x+1, y] if (x+1 < grid.length)
   end
-  def find_southeastern_neighbor
+  def find_southeastern_neighbor(x,y)
     [x+1, y+1] if (x+1 < grid.length && y+1 < grid.length)
   end
-  def find_eastern_neighbor
+  def find_eastern_neighbor(x,y)
     [x, y+1] if (y+1 < grid.length)
   end
-  def find_northeastern_neighbor
+  def find_northeastern_neighbor(x,y)
     [x-1, y+1] if (x-1 >= 0 && y+1 < grid.length)
   end
 end
--- spec/neighborhood_spec.rb
+++ spec/neighborhood_spec.rb
@@ -1,53 +1,52 @@
 require 'neighborhood'
 
 describe Neighborhood do
-  grid = [[1,1,0],[1,1,0],[1,0,1]]
+  before(:each) do
+    @grid = [[:living,:living,:dead],[:living,:living,:dead],[:living,:dead,:living]]
+    @neighborhood = Neighborhood.new(@grid)
+  end
+
   context "a neighborhood in a grid has neighbors" do
-    neighborhood = Neighborhood.new(grid, 1, 1)
 
     it "returns an array of the northern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_northern_neighbor).to eq([0,1])
+      expect(@neighborhood.find_northern_neighbor(1,1)).to eq([0,1])
     end
     it "returns an array of the northernwestern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_northernwestern_neighbor).to eq([0,0])
+      expect(@neighborhood.find_northernwestern_neighbor(1,1)).to eq([0,0])
     end
     it "returns an array of the western neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_western_neighbor).to eq([1,0])
+      expect(@neighborhood.find_western_neighbor(1,1)).to eq([1,0])
     end
     it "returns an array of the southwestern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_southwestern_neighbor).to eq([2,0])
+      expect(@neighborhood.find_southwestern_neighbor(1,1)).to eq([2,0])
     end
     it "returns an array of the southern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_southern_neighbor).to eq([2,1])
+      expect(@neighborhood.find_southern_neighbor(1,1)).to eq([2,1])
     end
     it "returns an array of the southeastern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_southeastern_neighbor).to eq([2,2])
+      expect(@neighborhood.find_southeastern_neighbor(1,1)).to eq([2,2])
     end
     it "returns an array of the eastern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_eastern_neighbor).to eq([1,2])
+      expect(@neighborhood.find_eastern_neighbor(1,1)).to eq([1,2])
     end
     it "returns an array of the northeastern neighbor coordinates for a neighborhood" do
-      expect(neighborhood.find_northeastern_neighbor).to eq([0,2])
+      expect(@neighborhood.find_northeastern_neighbor(1,1)).to eq([0,2])
     end
 
     it "creates an array with all the neighbors" do
-      expect(neighborhood.get_neighbors).to eq([[0,1],[0,0],[1,0],[2,0],[2,1],[2,2],[1,2],[0,2]])
+      expect(@neighborhood.get_neighbors(1,1)).to eq([[0,1],[0,0],[1,0],[2,0],[2,1],[2,2],[1,2],[0,2]])
     end
   end
 
   context "may have living neighbors" do
-    neighborhood = Neighborhood.new(grid, 0, 1)
-
     it "has three living neighbors" do
-      expect(neighborhood.number_of_living_neighbors).to eq(3)
+      expect(@neighborhood.number_of_living_neighbors(0,1)).to eq(3)
     end
   end
 
   context "may not have neighbors to some directions" do
-    neighborhood = Neighborhood.new(grid, 0, 0)
-
     it "has three living neighbors" do
-      expect(neighborhood.number_of_living_neighbors).to eq(3)
+      expect(@neighborhood.number_of_living_neighbors(0,0)).to eq(3)
     end
   end
 end
